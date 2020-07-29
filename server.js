d664637a6c7692a108b909c821fcc479fe4bae1c
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-07-29 03:43:58 +0000

    server on 5001, 5000 is used

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => res.send('API Running'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
