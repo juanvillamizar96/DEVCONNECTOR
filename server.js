a1899c3a7d3bd14796ef6802fcd840e5984035f0
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-07-29 04:04:17 +0000

    add connection to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//Connect Database
+connectDB();
+
 const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => res.send('API Running'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
