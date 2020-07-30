9e73a188de7b58245fbb01f8d9192f9c1f668c8d
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-07-30 04:25:14 +0000

    Profile API Routes

--- config/db.js
+++ config/db.js
@@ -7,6 +7,8 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB Connected...');
   } catch (err) {
