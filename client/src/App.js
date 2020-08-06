9ec58560345047766fe4e40b399778ddd9738d17
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-08-06 03:00:00 +0000

    Posts: Reducer, Action and initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-foms/AddExperience';
 import AddEducation from './components/profile-foms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -63,6 +64,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
